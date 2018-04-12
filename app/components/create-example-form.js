645c99b950593a40d3efbbb22e89b99b2d379682
ismurray <ians.murray@yahoo.com>
2018-04-12 10:24:57 -0400

    Add create example
    - generate create-example-form component
    - create new example pojo on component layer, then pass up to the route
    to createRecord and persist to the db

---
+++ app/components/create-example-form.js
@@ -0,0 +1,15 @@
+import Component from '@ember/component'
+
+export default Component.extend({
+  didInsertElement () {
+    this.set('newExample', {
+      text: ''
+    })
+  },
+  actions: {
+    createExample () {
+      console.log('new example is ', this.get('newExample'))
+      this.sendAction('createExample', this.get('newExample'))
+    }
+  }
+})
--- app/routes/examples.js
+++ app/routes/examples.js
@@ -3,5 +3,12 @@ import Route from '@ember/routing/route'
 export default Route.extend({
   model () {
     return this.get('store').findAll('example')
+  },
+  actions: {
+    addExample (examplePojo) {
+      console.log('in the route alfdsh')
+      const example = this.get('store').createRecord('example', examplePojo)
+      return example.save()
+    }
   }
 })
